278e412220d062a560650ab6b88f88512b8cd4af
Igor Alex <igoralex8701@gmail.com.br>
2023-02-26 14:38:52 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+  
+function minhaFuncao2()
+  {
+      var minhaInput=document.getElementById("meuInput");
+      var minhaInputVal = (minhaInput.value).toUpperCase();
+      minhaInput.value = minhaInputVal
+
+      var texto =  document.getElementById("texto").innerText = minhaInputVal;
+
+  }
